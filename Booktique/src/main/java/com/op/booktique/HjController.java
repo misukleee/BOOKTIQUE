292b94380709d1fb9b65122d7f1555f933508622
PC-20 <hjsdev23@gmail.com>
2024-08-07 16:30:03 +0900

    시큐리티

--- Booktique/src/main/java/com/op/booktique/HjController.java
+++
@@ -1,8 +0,0 @@
-package com.op.booktique;
-
-public class HjController {
-	int a;
-	int b;
-	int c;
-	int d;
-}
---
+++ Booktique/src/main/java/com/op/booktique/security/SecurityController.java
@@ -0,0 +1,8 @@
+package com.op.booktique.security;
+
+public class SecurityController {
+	int a;
+	int b;
+	int c;
+	int d;
+}
