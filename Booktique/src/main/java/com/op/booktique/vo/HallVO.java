da4fb32ff44855280ad9dad50f257e9188c41bf8
Da-hah <k.da.hees2@gmail.com>
2024-08-26 20:49:58 +0900

    공연장 VO 추가

---
+++ Booktique/src/main/java/com/op/booktique/vo/HallVO.java
@@ -0,0 +1,14 @@
+package com.op.booktique.vo;
+
+import lombok.Data;
+
+@Data
+public class HallVO {
+	private String hallSq;
+	private String hallName;
+	private String hallPost;
+	private String hallAddr;
+	private String hallDeAddr;
+	private int hallWidth;
+	//왜커밋이안될까
+}
